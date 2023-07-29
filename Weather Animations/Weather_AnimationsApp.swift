87395f9853642cfc4e18e4ec1ebe2c67d6110fdf
NazarStf <hworker98@gmail.com>
2023-07-29 17:10:29 +0300

    Initial Commit

---
+++ Weather Animations/Weather_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AnimationsApp.swift
+//  Weather Animations
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
