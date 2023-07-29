a5d9dd77f7aef88b2c4a48d6afe23209fe2b16ea
NazarStf <hworker98@gmail.com>
2023-07-29 17:15:32 +0300

    PickerView

--- Weather Animations/DataModel.swift
+++ Weather Animations/DataModel.swift
@@ -14,31 +14,31 @@ struct DataModel {
 	
 	//weekly temperature data
 	static let temperature: [DataModel] = [
-	   .init(day: "M", data: 0.7),
-	   .init(day: "T", data: 0.9),
-	   .init(day: "W", data: 0.8),
-	   .init(day: "TH", data: 0.75),
-	   .init(day: "F", data: 0.65),
-	   .init(day: "Sa", data: 0.85),
-	   .init(day: "Su", data: 0.9)]
-   
-   //weekly Precipitation data
-   static let precipitation: [DataModel] = [
-	   .init(day: "M", data: 0.6),
-	   .init(day: "T", data: 0.2),
-	   .init(day: "W", data: 0.3),
-	   .init(day: "TH", data: 0.4),
-	   .init(day: "F", data: 0.9),
-	   .init(day: "Sa", data: 0.5),
-	   .init(day: "Su", data: 0.4)]
-   
-   //weekly wind percentage data
-   static let wind: [DataModel] = [
-	   .init(day: "M", data: 0.4),
-	   .init(day: "T", data: 0.3),
-	   .init(day: "W", data: 0.3),
-	   .init(day: "TH", data: 0.5),
-	   .init(day: "F", data: 0.3),
-	   .init(day: "Sa", data: 0.2),
-	   .init(day: "Su", data: 0.8)]
+		.init(day: "M", data: 0.7),
+		.init(day: "T", data: 0.9),
+		.init(day: "W", data: 0.8),
+		.init(day: "TH", data: 0.75),
+		.init(day: "F", data: 0.65),
+		.init(day: "Sa", data: 0.85),
+		.init(day: "Su", data: 0.9)]
+	
+	//weekly Precipitation data
+	static let precipitation: [DataModel] = [
+		.init(day: "M", data: 0.6),
+		.init(day: "T", data: 0.2),
+		.init(day: "W", data: 0.3),
+		.init(day: "TH", data: 0.4),
+		.init(day: "F", data: 0.9),
+		.init(day: "Sa", data: 0.5),
+		.init(day: "Su", data: 0.4)]
+	
+	//weekly wind percentage data
+	static let wind: [DataModel] = [
+		.init(day: "M", data: 0.4),
+		.init(day: "T", data: 0.3),
+		.init(day: "W", data: 0.3),
+		.init(day: "TH", data: 0.5),
+		.init(day: "F", data: 0.3),
+		.init(day: "Sa", data: 0.2),
+		.init(day: "Su", data: 0.8)]
 }
